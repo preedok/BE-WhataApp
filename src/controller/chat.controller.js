5946a52eb27b395e500539bc0266442de99eeaab
preedok <88433068+Iqbalapredo@users.noreply.github.com>
2023-03-30 12:06:37 +0700

    Update chat.controller.js

--- src/controller/chat.controller.js
+++ src/controller/chat.controller.js
@@ -19,6 +19,28 @@ const chatController = {
       next(new createError.InternalServerError());
     }
   },
+  
+   deleteChat: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { id: sender } = req.decoded;
+
+      const result = await chatModel.deleteChat(id, sender);
+
+      if (result.rowCount === 0) {
+        return next(new createError.NotFound());
+      }
+      res.json({
+        msg: "chat berhasil dihapus",
+        data: {
+          chatId: id,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      next(new createError.InternalServerError());
+    }
+  },
 };
 
 module.exports = chatController;
