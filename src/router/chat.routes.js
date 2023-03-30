76b62942fe4b7b7b4728373c489a55bb579a33ac
preedok <88433068+Iqbalapredo@users.noreply.github.com>
2023-03-30 12:07:44 +0700

    Update chat.routes.js

--- src/router/chat.routes.js
+++ src/router/chat.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { AllChat } = require("../controller/chat.controller");
+const { AllChat, deleteChat } = require("../controller/chat.controller");
 const jwtAuth = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.get("/:id", jwtAuth, AllChat);
+router.delete("/:id", jwtAuth, deleteChat);
 
 module.exports = router;
